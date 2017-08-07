8d46f5ce6487a5e18abb74364432dfec622928f7
mrashno <me@rashno.org>
2017-08-07 11:00:24 +0430

    Initial commit

---
+++ FCM_Server_Project/FCM_Server_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCM_Server_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
