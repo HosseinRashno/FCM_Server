9775c02122d5c05111f5152160392bb0d6fada8a
mrashno <me@rashno.org>
2017-08-13 10:33:42 +0430

    Add serilization classes and other

--- FCM_Server_Project/FCM_Server_Project/Program.cs
+++ FCM_Server_Project/FCM_Server_Project/Program.cs
@@ -10,6 +10,26 @@ namespace FCM_Server_Project
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Type one of these commands: Topic/Id/Exit");
+                string command = Console.ReadLine();
+                switch (command.ToLower())
+                {
+                    case "topic":
+                        // TODO : Get information about message and send that to selected device id
+                        break;
+                    case "id":
+                        // TODO : Get information about message and send that to selected topic
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        Console.WriteLine("Your input was not correct! Try another one.");
+                        break;
+                }
+            }
         }
     }
 }
